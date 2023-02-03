2bf1cf82f2ad86080f765ab73e4f719fc509efcd
Inger <37039183+Selawen@users.noreply.github.com>
2023-02-03 23:33:29 +0100

    Made enemy AI
    Made a simple AI for the enemy to move around the maze

--- Assets/Scripts/BugAI.cs
+++ Assets/Scripts/BugAI.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class BugAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int stepTime = 1;
+
+    private static Vector2[] directions = { Vector2.up, Vector2.down, Vector2.right, Vector2.left };
+
+    private Vector2 lastDirection = Vector2.up;
+
+    [SerializeField] private LayerMask wallMask;
+     
+    private void Start()
     {
-        
+        StartCoroutine(TakeStep());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TakeStep()
     {
-        
+        yield return new WaitForSeconds(stepTime);
+
+        List<int> wallDirections = new List<int>();
+        List<int> openDirections = new List<int>();
+
+        for (int x = 0; x < 4; x++)
+        {
+            Debug.Log($"trying {x}");
+            RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, directions[x], 1f, wallMask);
+            if (hitinfo)
+            {
+                Debug.Log($"wall at direction {x}, {hitinfo.collider.name}");
+                wallDirections.Add(x);
+                continue;
+            }
+
+            openDirections.Add(x);
+        }
+
+
+        // keep going straight half the time
+        if (Random.Range(0, 2.0f) > 1 && !Physics2D.Raycast(transform.position, lastDirection, 1f, wallMask))
+        { }
+        else
+        {
+            if (openDirections.Count == 1)
+                lastDirection = directions[openDirections[0]];
+            else
+            {
+                int direction;
+
+                do
+                {
+                    direction = Random.Range(0, openDirections.Count);
+                } while (!(Random.Range(0, 2.0f) > 1 && (directions[openDirections[direction]] != lastDirection * -1)));
+
+                lastDirection = directions[openDirections[direction]];
+            }
+        }
+
+        transform.Translate(new Vector3(lastDirection.x, lastDirection.y, 0));
+
+        StartCoroutine(TakeStep());
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Physics2D.Raycast(transform.position, lastDirection, 1f, wallMask))
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(lastDirection.x, lastDirection.y, 0));
     }
 }
